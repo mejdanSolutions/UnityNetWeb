cc03162d7707e4833e54e05cc3360e109816a39a
Faruk Spahic <farukspahictz@gmail.com>
2023-04-08 19:00:24 +0200

    Changed tailwind config to output to build folder instead of dist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  options: {
+    safelist: [],
+    output: "./build/index.css",
+  },
   theme: {
     extend: {},
   },
