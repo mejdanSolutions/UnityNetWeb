c5d79ed7448a6e5b9c5423337608b6af3f931cee
Faruk Spahic <farukspahictz@gmail.com>
2023-04-08 19:54:42 +0200

    Created notifications modal

---
+++ src/modals/Notifications.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { getNotifications } from "../redux/notificationSlice";
+import Notification from "../cards/Notification";
+
+const Notifications = () => {
+  const dispatch = useAppDispatch();
+  const notifications = useAppSelector(
+    (state) => state.notification.notifications
+  );
+
+  console.log(notifications);
+
+  useEffect(() => {
+    dispatch(getNotifications());
+  }, [dispatch]);
+
+  return (
+    <div className="absolute right-[-3rem] top-[3.2rem] bg-white text-black shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-md p-2 z-20">
+      <h2 className="font-bold text-[1.2rem] text-center">Notifications</h2>
+      <div className="flex flex-col space-y-2 mt-2 w-[15rem] overflow-y-auto h-[15rem]">
+        {notifications.length === 0 && (
+          <span className="text-[0.9rem] text-center text-blue-500">
+            You dont have any notifications
+          </span>
+        )}
+        {notifications.map((notification) => (
+          <Notification
+            key={notification.notification_id}
+            receiverId={notification.id}
+            firstName={notification.first_name}
+            lastName={notification.last_name}
+            image={notification.image}
+            type={notification.type}
+            postId={notification.post_id}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Notifications;
