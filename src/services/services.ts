f2ebb0a2bb4c0a6069c325e4667b4c3b1b8e6514
Faruk Spahic <farukspahictz@gmail.com>
2023-04-09 13:45:06 +0200

    Created services

---
+++ src/services/services.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export const getUserInfo = async (userId: number) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:7000/api/users/getUserInfo/${userId}`
+    );
+
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getPostCommentsCount = async (postId: number) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:7000/api/comments/getPostCommentsCount/${postId}`
+    );
+
+    return response.data;
+  } catch (err) {}
+};
+
+export const getPhotoCommentsCount = async (id: number) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:7000/api/photos/getPhotoCommentsCount/${id}`
+    );
+
+    return response.data;
+  } catch (err) {}
+};
