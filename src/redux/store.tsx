b46a19f3922c9e138b99d9d516729cdbafc82102
Faruk Spahic <farukspahictz@gmail.com>
2023-04-09 16:37:09 +0200

    Removed unused slice imports

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../redux/authSlice";
 import postReducer from "../redux/postSlice";
 import chatReducer from "../redux/chatSlice";
-import socketReducer from "../redux/socketSlice";
 import friendRequestReducer from "../redux/friendRequestSlice";
 import notificationReducer from "../redux/notificationSlice";
 
@@ -11,7 +10,6 @@ const store = configureStore({
     auth: authReducer,
     post: postReducer,
     chat: chatReducer,
-    socket: socketReducer,
     request: friendRequestReducer,
     notification: notificationReducer,
   },
