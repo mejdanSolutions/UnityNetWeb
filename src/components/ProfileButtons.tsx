16a31b6d67a0f69d8910adc7f3004bb3c2f12957
Faruk Spahic <farukspahictz@gmail.com>
2023-04-22 15:59:15 +0200

    Refactored Profile page and added a new component "ProfileButtons" that includes some of the code from the profile page

---
+++ src/components/ProfileButtons.tsx
@@ -0,0 +1,154 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { sendFriendRequest } from "../redux/friendRequestSlice";
+import { useAppDispatch } from "../redux/hooks";
+import { User } from "../types/types";
+import { useAppSelector } from "../redux/hooks";
+import { GrFormCheckmark } from "react-icons/gr";
+
+interface Request {
+  status: boolean;
+  receiver: number;
+  sender: number;
+}
+
+interface Props {
+  setFriendStatus: React.Dispatch<React.SetStateAction<boolean>>;
+  friendStatus: boolean;
+  userInfo: User;
+}
+
+const ProfileButtons = ({ setFriendStatus, friendStatus, userInfo }: Props) => {
+  const dispatch = useAppDispatch();
+  const [friendReqStatus, setFriendReqStatus] = useState<Request>({
+    status: false,
+    receiver: 0,
+    sender: 0,
+  });
+
+  console.log("userId", userInfo.id);
+
+  console.log(friendReqStatus);
+
+  const loggedUserInfo = useAppSelector((state) => state.auth.loggedUserInfo);
+
+  const acceptRequestHandler = async () => {
+    try {
+      await axios.get(
+        `http://localhost:7000/api/followers/acceptFriendRequest/${friendReqStatus.sender}`
+      );
+
+      setFriendStatus(true);
+      setFriendReqStatus({ status: false, receiver: 0, sender: 0 });
+    } catch (err) {}
+  };
+
+  const friendRequestHandler = async () => {
+    dispatch(sendFriendRequest({ request: 1, receiverId: userInfo.id }));
+
+    try {
+      await axios.get(
+        `http://localhost:7000/api/followers/sendFriendRequest/${userInfo.id}`
+      );
+
+      getFriendReqStatus();
+    } catch (err) {}
+  };
+
+  const unfriendHandler = async () => {
+    try {
+      await axios.get(
+        `http://localhost:7000/api/followers/removeFromFriends/${userInfo.id}`
+      );
+      setFriendStatus(false);
+    } catch (err) {}
+  };
+
+  const getFriendReqStatus = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:7000/api/followers/checkFriendRequestStatus/${userInfo.id}`
+      );
+
+      setFriendReqStatus(response.data);
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    if (loggedUserInfo.id !== userInfo.id) {
+      getFriendReqStatus();
+    }
+  }, [loggedUserInfo, userInfo]);
+
+  useEffect(() => {
+    const getFriendStatus = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:7000/api/followers/checkFriendsStatus/${userInfo.id}`
+        );
+
+        setFriendStatus(response.data);
+      } catch (err) {}
+    };
+
+    getFriendStatus();
+  }, [userInfo]);
+
+  return (
+    <div className="ml-5">
+      {loggedUserInfo.id !== userInfo.id &&
+        !friendStatus &&
+        friendReqStatus.status === false && (
+          <button
+            onClick={friendRequestHandler} //
+            type="submit"
+            className="bg-blue-500 text-white p-2 rounded-md font-bold hover:bg-blue-600"
+          >
+            Add to friends
+          </button>
+        )}
+
+      {friendStatus && loggedUserInfo.id !== userInfo.id && (
+        <div className="flex items-center space-x-2">
+          <GrFormCheckmark />
+          <span>Friends</span>
+          <button
+            onClick={unfriendHandler}
+            type="submit"
+            className="bg-blue-500 text-white p-1 rounded-md font-bold hover:bg-blue-600"
+          >
+            Unfriend
+          </button>
+        </div>
+      )}
+
+      {friendReqStatus.status &&
+        friendReqStatus.sender === loggedUserInfo.id && (
+          <div className="flex items-center space-x-1">
+            {" "}
+            <GrFormCheckmark className="" />
+            <span className="text-blue-600">Friend request sent</span>
+          </div>
+        )}
+
+      {friendReqStatus.status &&
+        friendReqStatus.receiver === loggedUserInfo.id && (
+          <div className="flex space-x-2">
+            <button
+              onClick={acceptRequestHandler}
+              type="submit"
+              className="bg-blue-500 text-white p-1 rounded-md font-bold hover:bg-blue-600"
+            >
+              Accept
+            </button>
+
+            <button className="border-2 border-blue-500 rounded-md text-blue-600 p-1">
+              Reject
+            </button>
+          </div>
+        )}
+    </div>
+  );
+};
+
+export default ProfileButtons;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,43 +1,20 @@
 import React, { useEffect, useState } from "react";
-import Friend from "../cards/Friend";
 import { useParams } from "react-router-dom";
 import { User } from "../types/types";
 import axios from "axios";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
-import { GrFormCheckmark } from "react-icons/gr";
-// import { IPost } from "../types/types";
 import Post from "../cards/Post";
-
 import { fetchUserPosts } from "../redux/postSlice";
-import UserInfo from "../components/UserInfo";
 import Navbar from "../components/Navbar";
-import { sendFriendRequest } from "../redux/friendRequestSlice";
 import ProfileInfo from "../components/ProfileInfo";
 import ProfileFriends from "../components/ProfileFriends";
 import ProfileHeader from "../components/ProfileHeader";
-const profileDefault = require("../images/profile.jpg");
-
-// interface IUPost extends IPost {
-//   id: number;
-//   first_name: string;
-//   last_name: string;
-// }
-
-interface Request {
-  status: boolean;
-  receiver: number;
-  sender: number;
-}
-
-interface IUser extends User {
-  cover_image: string;
-}
+import ProfileButtons from "../components/ProfileButtons";
 
 const Profile = () => {
   const [page, setPage] = useState(1);
   const dispatch = useAppDispatch();
-  const loggedUserInfo = useAppSelector((state) => state.auth.loggedUserInfo);
-  const [userInfo, setUserInfo] = useState<IUser>({
+  const [userInfo, setUserInfo] = useState<User>({
     id: 0,
     first_name: "",
     last_name: "",
@@ -45,13 +22,7 @@ const Profile = () => {
     cover_image: "",
   });
   const [friendStatus, setFriendStatus] = useState(false);
-
   const posts = useAppSelector((state) => state.post.userPosts);
-  const [friendReqStatus, setFriendReqStatus] = useState<Request>({
-    status: false,
-    receiver: 0,
-    sender: 0,
-  });
 
   console.log(friendStatus);
 
@@ -77,147 +48,36 @@ const Profile = () => {
     };
   }, [page]);
 
-  const acceptRequestHandler = async () => {
-    try {
-      await axios.get(
-        `http://localhost:7000/api/followers/acceptFriendRequest/${friendReqStatus.sender}`
-      );
-
-      setFriendStatus(true);
-      setFriendReqStatus({ status: false, receiver: 0, sender: 0 });
-    } catch (err) {}
-  };
-
   useEffect(() => {
     dispatch(fetchUserPosts({ userId, page }));
   }, [page]);
 
-  const getFriendReqStatus = async () => {
-    try {
-      const response = await axios.get(
-        `http://localhost:7000/api/followers/checkFriendRequestStatus/${userId}`
-      );
-
-      setFriendReqStatus(response.data);
-    } catch (err) {}
-  };
-
-  useEffect(() => {
-    getFriendReqStatus();
-  }, []);
-
-  const unfriendHandler = async () => {
-    try {
-      await axios.get(
-        `http://localhost:7000/api/followers/removeFromFriends/${userInfo.id}`
-      );
-      setFriendStatus(false);
-    } catch (err) {}
-  };
-
-  useEffect(() => {
-    const getFriendStatus = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:7000/api/followers/checkFriendsStatus/${userInfo?.id}`
-        );
-
-        setFriendStatus(response.data);
-      } catch (err) {}
-    };
-
-    getFriendStatus();
-  }, [userId, userInfo]);
-
-  const friendRequestHandler = async () => {
-    dispatch(sendFriendRequest({ request: 1, receiverId: userInfo.id }));
-
-    try {
-      await axios.get(
-        `http://localhost:7000/api/followers/sendFriendRequest/${userInfo.id}`
-      );
-
-      getFriendReqStatus();
-    } catch (err) {}
-  };
-
   useEffect(() => {
     const getUser = async () => {
       const response = await axios.get(
-        `http://localhost:7000/api/users/getUserInfo/${id}`
+        `http://localhost:7000/api/users/getUserInfo/${userId}`
       );
 
       setUserInfo(response.data);
     };
 
     getUser();
-  }, [id]);
+  }, [userId]);
 
   return (
     <>
       <Navbar />
       <section className="max-w-5xl mx-auto shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-gray-100">
         <ProfileHeader userInfo={userInfo} />
-        <div className="my-4 mt-20 ml-3 w-max text-center">
-          <h2 className="text-2xl font-bold">
+        <div className="my-4 mt-20 w-max text-center">
+          <h2 className="text-2xl font-bold ml-5">
             {userInfo.first_name} {userInfo.last_name}
           </h2>
-
-          <div className="">
-            {loggedUserInfo.id !== userId &&
-              !friendStatus &&
-              friendReqStatus.status === false && (
-                <button
-                  onClick={friendRequestHandler} //
-                  type="submit"
-                  className="bg-blue-500 text-white p-2 rounded-md font-bold hover:bg-blue-600"
-                >
-                  Add to friends
-                </button>
-              )}
-
-            {friendStatus && loggedUserInfo.id !== userId && (
-              <div className="flex items-center space-x-2">
-                <GrFormCheckmark />
-                <span>Friends</span>
-                <button
-                  onClick={unfriendHandler}
-                  type="submit"
-                  className="bg-blue-500 text-white p-1 rounded-md font-bold hover:bg-blue-600"
-                >
-                  Unfriend
-                </button>
-              </div>
-            )}
-
-            {friendReqStatus.status === true &&
-              friendReqStatus.receiver === userId &&
-              loggedUserInfo.id !== userId && (
-                <div className="flex items-center space-x-2">
-                  {" "}
-                  <GrFormCheckmark className="" />
-                  <span className="text-blue-600">Friend request sent</span>
-                </div>
-              )}
-
-            {friendReqStatus.status &&
-              loggedUserInfo.id !== userId &&
-              friendReqStatus.sender === userId && (
-                <div className="flex space-x-2">
-                  <button
-                    onClick={acceptRequestHandler}
-                    type="submit"
-                    className="bg-blue-500 text-white p-1 rounded-md font-bold hover:bg-blue-600"
-                  >
-                    Accept
-                  </button>
-
-                  <button className="border-2 border-blue-500 rounded-md text-blue-600 p-1">
-                    Reject
-                  </button>
-                </div>
-              )}
-          </div>
+          <ProfileButtons
+            userInfo={userInfo}
+            setFriendStatus={setFriendStatus}
+            friendStatus={friendStatus}
+          />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2">
           <div className="w-full mt-6">
